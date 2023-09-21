2022f53f3b9d3348af89f27a871d72dff5388d7d
Abhimanyu0924 <abhimanyu.singh2020@vitstudent.ac.in>
2023-09-21 17:12:27 +0530

    second commit

--- app/routes/qrcodes.$id.jsx
+++ app/routes/qrcodes.$id.jsx
@@ -1,26 +1,31 @@
-import {json} from "@remix-run/node";
+import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { useLoaderData } from "@remix-run/react";
-import db from "../db.server"
+
+import db from "../db.server";
 import { getQRCodeImage } from "~/models/QRCode.server";
 
+export const loader = async ({ params }) => {
+  invariant(params.id, "Could not find QR code destination");
 
-export const loader = async({params})=>{
-    invariant(params.id,"could not find QR Code destination");
-    const id = Number(params.id);
-    const qrCode = await db.qRCode.findFirst({where:{id}});
-    return json({
-        title:qrCode.title,
-        image:await getQRCodeImage(id)
-    });
+  const id = Number(params.id);
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  return json({
+    title: qrCode.title,
+    image: await getQRCodeImage(id),
+  });
 };
 
-export defualt function QRCode() {
-    const {image,title} = useLoaderData();
-    return(
-        <>
-            <h1>{title}</h1>
-            <img src={image} alt= {'QR code for the pruduct'} />
-        </>
-    );    
+export default function QRCode() {
+  const { image, title } = useLoaderData();
+
+  return (
+    <>
+        <h1>{title}</h1>
+        <img src={image} alt={"QR Code for product"} />
+    </>
+    );
 }
